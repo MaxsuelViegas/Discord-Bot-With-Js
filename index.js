ccc7cd6b69f6d7946ffacbb424af237dc26266ca
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-20 23:21:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -35,4 +35,18 @@ for (const file of slashFiles){
     client.slashcommands.set(slashcmd.data.name, slashcmd)
     if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
 }
-
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
