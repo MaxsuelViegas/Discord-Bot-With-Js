2a43b7e19c1a54ab6ef63ec230aaf85f0e56b096
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-23 20:11:39 -0300

    Update pause.js

--- pause.js
+++ pause.js
@@ -1,1 +1,13 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+	data: new SlashCommandBuilder().setName("pausa").setDescription("Pausa a música"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem música na fila")
+
+		queue.setPaused(true)
+        await interaction.editReply("Música foi pausada! Use `/resume` para continuar a reproduzir")
+	},
+}
