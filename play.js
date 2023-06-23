336f60e187f256ea8f8d7ab3b381085c2d830001
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-23 20:09:50 -0300

    Update play.js

--- play.js
+++ play.js
@@ -2,3 +2,28 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
 
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("play")
+		.setDescription("Toca uma música do YouTube")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("Música")
+				.setDescription("Toca uma música por url")
+				.addStringOption((option) => option.setName("url").setDescription("url da música").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("playlist")
+				.setDescription("Toca uma Playlist via url")
+				.addStringOption((option) => option.setName("url").setDescription("playlist's url").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("Busca")
+				.setDescription("Buscar músicas no youtube")
+				.addStringOption((option) =>
+					option.setName("Buscar").setDescription("Busca").setRequired(true)
+				)
+		),
+
