a7d5ad4b4350053575f520a0afa43b3225205859
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-28 20:57:11 -0300

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,1 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+module.exports = {
+	data: new SlashCommandBuilder().setName("Random").setDescription("randomiza a fila"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem músicas na fila")
+
+		queue.shuffle()
+        await interaction.editReply(`A fila de ${queue.tracks.length} músicas foi randomizada!`)
+	},
+}
