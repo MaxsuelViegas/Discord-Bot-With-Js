b1fecc3e094a91a947708d2d493e2b1b1d6d7d74
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-29 12:11:56 -0300

    Update skipto.js

--- skipto.js
+++ skipto.js
@@ -1,1 +1,18 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+module.exports = {
+	data: new SlashCommandBuilder().setName("Pular Para").setDescription("Pula para a música selecionada #")
+    .addNumberOption((option) => 
+        option.setName("Numero Fila").setDescription("Pular para").setMinValue(1).setRequired(true)),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem música na fila")
+
+        const trackNum = interaction.options.getNumber("Numero Fila")
+        if (trackNum > queue.tracks.length)
+            return await interaction.editReply("Numero de fila invalido")
+		queue.skipTo(trackNum - 1)
+
+        await interaction.editReply(`Skipou para o numero de fila ${trackNum}`)
+	},
+}
