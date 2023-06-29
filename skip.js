dadf9540e03e4bd619327d7a00aee823b3ea22ee
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-29 12:13:13 -0300

    Update skip.js

--- skip.js
+++ skip.js
@@ -1,2 +1,19 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
+module.exports = {
+	data: new SlashCommandBuilder().setName("Pular").setDescription("Skipa a música atual"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
+
+		if (!queue) return await interaction.editReply("Não tem música na fila")
+
+        const currentSong = queue.current
+
+		queue.skip()
+        await interaction.editReply({
+            embeds: [
+                new MessageEmbed().setDescription(`${currentSong.title} Foi skipada!`).setThumbnail(currentSong.thumbnail)
+            ]
+        })
+	},
+}
