f87c2895c62f66af8ce2ab4b0725b83b408c89d8
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-30 20:54:51 -0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
+
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("Fila")
     .setDescription("Mostra a música que está na fila")
     .addNumberOption((option) => option.setName("pag").setDescription("Número de pag da fila").setMinValue(1)),
+    
  run: async ({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guildId)
         if (!queue || !queue.playing){
