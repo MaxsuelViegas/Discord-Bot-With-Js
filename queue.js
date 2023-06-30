13f74cbac2fd9f42a1ac737e653b4fb6c7e67fb6
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-06-30 20:54:38 -0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -5,3 +5,8 @@ module.exports = {
     .setName("Fila")
     .setDescription("Mostra a música que está na fila")
     .addNumberOption((option) => option.setName("pag").setDescription("Número de pag da fila").setMinValue(1)),
+ run: async ({ client, interaction }) => {
+        const queue = client.player.getQueue(interaction.guildId)
+        if (!queue || !queue.playing){
+            return await interaction.editReply("Não tem músicas na fila")
+        }
