ecaba0f7c79413f7d6e8c7f0734544cd16375c50
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-01 22:01:43 -0300

    Update queue.js

--- queue.js
+++ queue.js
@@ -9,15 +9,19 @@ module.exports = {
     
  run: async ({ client, interaction }) => {
         const queue = client.player.getQueue(interaction.guildId)
+     
         if (!queue || !queue.playing){
             return await interaction.editReply("Não tem músicas na fila")
         }
+     
  const totalPages = Math.ceil(queue.tracks.length / 10) || 1
+     
  const page = (interaction.options.getNumber("pag") || 1) - 1
+     
 if (page > totalPages) 
             return await interaction.editReply(`Pagina invalida. Tem somente ${totalPages} paginas na fila`)
 
         const queueString = queue.tracks.slice(page * 10, page * 10 + 10).map((song, i) => {
-            return `**${page * 10 + i + 1}.** \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`
-        }).join("\n")
+     return `**${page * 10 + i + 1}.** \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`}).join("\n")
+     
         const currentSong = queue.current
