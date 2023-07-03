192dffe6ebd1588bb4a2bdc8125ef2fd309774a2
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-03 20:24:59 -0300

    Update play.js

--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
 const { MessageEmbed } = require("discord.js")
+
 const { QueryType } = require("discord-player")
 
 module.exports = {
