a23b0dfc4c44bd5e4d0a636de4940ebfcf35d27e
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-08 12:22:18 -0300

    Update info.js

--- info.js
+++ info.js
@@ -5,3 +5,7 @@ module.exports = {
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 if (!queue) return await interaction.editReply("Não tem música na fila")
+	let bar = queue.createProgressBar({
+			queue: false,
+			length: 19,
+		})
