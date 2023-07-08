555d5cd6a112eb4777b9ae1acd2f81bcec86f9e6
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-08 12:22:11 -0300

    Update info.js

--- info.js
+++ info.js
@@ -4,3 +4,4 @@ module.exports = {
 	data: new SlashCommandBuilder().setName("info").setDescription("Mostra info da musica que está tocando"),
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
+if (!queue) return await interaction.editReply("Não tem música na fila")
