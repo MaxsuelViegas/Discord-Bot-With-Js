936ae03a81352176af20093418e06b0bdfdf9900
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-08 12:21:44 -0300

    Update info.js

--- info.js
+++ info.js
@@ -1,2 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
+module.exports = {
+	data: new SlashCommandBuilder().setName("info").setDescription("Mostra info da musica que está tocando"),
+	run: async ({ client, interaction }) => {
+		const queue = client.player.getQueue(interaction.guildId)
