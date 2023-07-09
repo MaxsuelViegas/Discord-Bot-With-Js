53ef7318b5e13ffdc65b1c274deabc8066d84988
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-09 20:48:40 -0300

    Update info.js

--- info.js
+++ info.js
@@ -7,6 +7,7 @@ module.exports = {
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 if (!queue) return await interaction.editReply("Não tem música na fila")
+		
 	let bar = queue.createProgressBar({
 			queue: false,
 			length: 19,
@@ -16,7 +17,9 @@ if (!queue) return await interaction.editReply("Não tem música na fila")
 
 		await interaction.editReply({
 			embeds: [new MessageEmbed()
+				 
             .setThumbnail(song.thumbnail)
+				 
             .setDescription(`Tocando agora [${song.title}](${song.url})`)
         ],
 		})
