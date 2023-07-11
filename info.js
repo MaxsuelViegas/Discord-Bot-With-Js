1eb288592d87bd59ff86a11520442c9cebb984fa
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-11 17:42:02 -0300

    Update info.js

--- info.js
+++ info.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 
 module.exports = {
-	data: new SlashCommandBuilder().setName("info").setDescription(""),
+	data: new SlashCommandBuilder().setName("info").setDescription("Show info of the song that is playing"),
 	run: async ({ client, interaction }) => {
 		const queue = client.player.getQueue(interaction.guildId)
 if (!queue) return await interaction.editReply("Não tem música na fila")
