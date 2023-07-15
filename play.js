f95113cc527bc8fd1cab7a7cbb5731f228d98cf0
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-15 16:32:02 -0300

    Update play.js

--- play.js
+++ play.js
@@ -17,7 +17,7 @@ module.exports = {
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName("playlist")
-				.setDescription(" url")
+				.setDescription("Play a playlist by url")
 				.addStringOption((option) => option.setName("url").setDescription("playlist's url").setRequired(true))
 		)
 		.addSubcommand((subcommand) =>
