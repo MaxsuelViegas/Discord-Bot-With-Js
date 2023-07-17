0a925749ed214f90aa1cf95baff152b5a778d1e9
Max <116040038+MaxsuelViegas@users.noreply.github.com>
2023-07-17 08:37:45 -0300

    Update pause.js

--- pause.js
+++ pause.js
@@ -7,7 +7,7 @@ module.exports = {
 		
 		const queue = client.player.getQueue(interaction.guildId)
 
-		if (!queue) return await interaction.editReply("")
+		if (!queue) return await interaction.editReply("No song")
 
 		queue.setPaused(true)
         await interaction.editReply("Music has been paused! Use `/resume` to play it again!")
